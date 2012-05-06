bb10673d30d1a1a3900c19ef410fe993caa38d2d
Prabesh Shrestha <prabesh708@gmail.com>
2012-05-06 13:51:29 +0200

    initial code commit .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipevaluation::Application.config.secret_token = '2c08234f4c597da1525da4137db0d2d8fa9467ab2b321d93b2edbc467eb61f46a4135db0f83ab6b43dd9a1fcc8654a77927f807401b00846e1ee31ce2b0c3bcc'
